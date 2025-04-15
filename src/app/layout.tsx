91d424dfc895e9125ff602fcd866816550323b98
Radexman <radoslaw.siek-web.dev@wp.pl>
2025-04-15 19:42:46 +0200

    Tailwind Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,25 @@
-import { PrismicPreview } from '@prismicio/next';
-import { repositoryName } from '@/prismicio';
+import localFont from "next/font/local";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+import "./globals.css";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  weight: "100 900",
+  display: "swap",
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body>{children}</body>
-			<PrismicPreview repositoryName={repositoryName} />
-		</html>
-	);
+  return (
+    <html lang="en" className={`${alpino.className}`}>
+      <body>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
 }
